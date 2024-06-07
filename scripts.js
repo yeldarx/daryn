7a44adcb39fe3a5eebfda00b710e6eaf591fd76f
Yeldar Kudaibergenov <mail@yeldar.org>
2024-06-08 02:08:27 +0500

    change some text

--- scripts.js
+++ scripts.js
@@ -21,13 +21,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const discountedPrice = meters >= 10 ? totalPrice * 0.9 : null;
 
         const resultHTML = `
-            <p>Наша цена за ${meters} погонных метра искуственного камня марки ${stoneType.value} от <span class="text-success">${totalPrice.toLocaleString()}</span> теңге.</p>
-            ${discountedPrice ? `<p class="discounted-price text-success">Цена со скидкой: от ${discountedPrice.toLocaleString()} теңге - скидка 10% при заказе от 10 пог.м.</p>` : ''}
-            <p>Цена того же камня если закажете через посредника от <span class="text-danger">${intermediaryPrice.toLocaleString()} теңге</span>.</p>
+            <p>Наша цена за ${meters} погонных метра искуственного камня марки ${stoneType.value} от <span class="text-success">${totalPrice.toLocaleString()}</span> теңге *</p>
+            ${discountedPrice ? `<p class="discounted-price text-success">Цена со скидкой: от ${discountedPrice.toLocaleString()} теңге - скидка 10% при заказе от 10 пог.м. *</p>` : ''}
+            <p>Цена того же камня если закажете через посредника от <span class="text-danger">${intermediaryPrice.toLocaleString()} теңге</span> *</p>
         `;
 
         result.innerHTML = resultHTML;
-        warning.innerHTML = '*Это примерные цены, точную цену уточняйте у консультанта.';
+        warning.innerHTML = '*Это примерные цены. Точную цену уточняйте у консультанта по телефону.';
     }
 
     // Слушатель изменений в поле ввода и выборе типа камня
